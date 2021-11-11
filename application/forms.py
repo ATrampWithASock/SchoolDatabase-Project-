08c2aa8abb61be3f1072cf9af4eec5950357d2c2
ATrampWithASock <J.lindop98@gmail.com>
2021-11-11 10:23:31 +0000

    added the application folder which contains the templates, init, forms, models and routes files

---
+++ application/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SelectField, SubmitField
+
+class AddStudent(FlaskForm):
+    student_name = StringField("Name")
+    submit = SubmitField("Add Student")
+
+
+class Marks(FlaskForm):
+    subject = SelectField("Subject", choices=[("physics","Physics"), ("chemistry","Chemistry"), ("biology","Biology"), ("maths","Maths"), ("english","English"), ("spanish","Spanish"), ("french","French"), ("geography","Geography"), ("history","History"), ("IT","IT"), ("sport_science","Sport Science")])
+    marks = IntegerField("Marks")
+    submit = SubmitField("Add Marks")
+
+
+class EditStudent(FlaskForm):
+    student_name = StringField("Name")
+    submit = SubmitField("Edit Student")
+
+
+class EditMarks(FlaskForm):
+    subject = SelectField("Subject", choices=[("physics","Physics"), ("chemistry","Chemistry"), ("biology","Biology"), ("maths","Maths"), ("english","English"), ("spanish","Spanish"), ("french","French"), ("geography","Geography"), ("history","History"), ("IT","IT"), ("sport_science","Sport Science")])
+    marks = IntegerField("Marks")
+    submit = SubmitField("Edit Marks")
---
+++ application/models.py
@@ -0,0 +1,17 @@
+from application import db
+
+
+class Student(db.Model):
+    """Students"""
+    ___tablename___ = "students"
+    student_ID = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+
+
+class Marks(db.Model):
+    """Marks"""
+    ___tablename___ = "marks"
+    mark_ID = db.Column(db.Integer, primary_key = True)
+    student_ID = db.Column(db.Integer, db.ForeignKey("students.student_ID"))
+    subject = db.Column(db.String(50))
+    marks = db.Column(db.Integer)
